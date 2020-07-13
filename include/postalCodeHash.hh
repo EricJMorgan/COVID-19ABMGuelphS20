99faf0d4dd9694acd8fd29369c8264c7e326810f
JamesCodes <buschj@uoguelph.ca>
2020-07-13 09:29:13 -0400

    Added some commejnts

--- include/postalCodeHash.hh
+++ include/postalCodeHash.hh
@@ -5,16 +5,61 @@
 
 class PostalCodeHash{
     private:
+    /**
+     * getPostalCode
+     * 
+     * This takes in the full address string given from the google places API
+     * and gets just the postal code from it. 
+     * NOTE, as of now the format by default is incorrect as it has the province 
+     * and the postal code not sepreated by a comma so that must be changed before using
+     * anything
+     * 
+     * @param fullAddress, the address in the google api printout format
+     * @return the string of the postal Code in the format LNL NLN
+     */
     string getPostalCode(string fullAddress);
+    /**
+     * placePostalInHash
+     * 
+     * This function takes in the postal code and hash table size and
+     * places the postal code into the hash table
+     * 
+     * @param newPostalCode, string of the postal code
+     * @param hashSize, the size of the hash table
+     */
     void placePostalInHash(string newPostalCode, int hashSize);
+    /**
+     * placePostalInHash
+     * 
+     * This function takes in the postal code, location type and hash table size and
+     * places the postal code into the hash table
+     * 
+     * @param newPostalCode, string of the postal code
+     * @param locationName, string of the location type.
+     * @param hashSize, the size of the hash table
+     */
     void placePostalInHash(string newPostalCode, string locationName, int hashSize);
+    /**
+     * openFile
+     * 
+     * This fucktion takes in a file name,
+     * opens and checks it validity before returns a ifstream object
+     * 
+     * @param fileName, name of the wanted file
+     * @return the ifstream object of the file, will be closed if invalid
+     */
     ifstream openFile(string fileName);
 
     public:
+    /**
+     * PostalCodeHash
+     * 
+     * This is the constructor for the postalcode hash object
     PostalCodeHash(string tsvFile, string evenMoreLocations, int hashSize);
     ~PostalCodeHash();
-    Location *hashTable;
     static int getPostalHash(int hashSize, string postalTSVToHash);
+
+    Location *hashTable;
 };
 
 #endif
