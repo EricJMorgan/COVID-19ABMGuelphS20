fbb777633990213e5c89d85e8db6f7df4e06400a
joyglitch <jli43@uoguelph.ca>
2020-07-17 10:53:24 -0400

    Added documentation to Hospital functions

--- include/hospital.hh
+++ include/hospital.hh
@@ -1,7 +1,7 @@
  /****************
  * COVID-19ABMGuelphS20
- * 06/07/20
- * ver 0.02
+ * 17/07/20
+ * ver 0.03
  * 
  * This is the header file for the hospital class
  ***************/
@@ -15,21 +15,93 @@
 
 using namespace std;
 
+//22 intensive care
+//62 surgery
+//8 Paediatrics/children
+//68 medicine
+//22 obsterics/childbirth
+
 //Declare simulation class
 class Hospital {
     private:
-    int totalBedCount;
-    int icuBedCount;
+    int totalBedCount = 130;
+    int icuBedCount = 22;
     int numberPpl;
     int icuCount;
 
     public:
+    /**
+     * Hospital
+     * 
+     * This is the default constructor for the location object
+     */
     Hospital();
-    void FreeIcuBeds();
-    void FreeHospitalBeds();
-    void IndicateOverflow();
-    void IncreaseHospitalCount();
-    void increaseIcuCount();
+
+    /**
+     * freeIcuBeds
+     * 
+     * This function frees up ICU beds upon patient's recovery or death
+     * 
+     * @param freeNum, this is the number of beds to be freed
+     */
+    void freeIcuBeds(int freeNum);
+
+    /**
+     * freeHospitalBeds
+     * 
+     * This function frees up hospital beds (excluding ICU beds) upon patient's
+     * recovery or death
+     * 
+     * @param freeNum, this is the number of beds to be freed
+     */
+    void freeHospitalBeds(int freeNum);
+
+    /**
+     * indicateOverflow
+     * 
+     * This function checks if the hospital has exceeded its bed limitations for
+     * either ICU or non-ICU beds; the simulation continues to run even when there
+     * is an overflow
+     */
+    void indicateOverflow();
+
+    /**
+     * increaseHospitcalCount
+     * 
+     * This function adds agents to the hospital if they are in need of hospital
+     * care
+     * 
+     * @param numAgents, this is the number of agents to be emitted to the hospital
+     */
+    void increaseHospitalCount(int numAgents);
+
+    /**
+     * increaseIcuCount
+     * 
+     * This function adds agents from the hospital to ICU if they are in need of
+     * critical care
+     * 
+     * @param numAgents, this is the number of agents to be emitted to ICU
+     */
+    void increaseIcuCount(int numAgents);
+
+    /**
+     * getTotalBeds
+     * 
+     * This function returns the number of agents using hospital beds
+     * 
+     * @return the number of agents in hospital care, excluding ICU cases
+     */
+    int getTotalBeds();
+
+    /**
+     * getIcuBeds
+     * 
+     * This function returns the number of agents in ICU care
+     * 
+     * @return the number of agents in ICU cases
+     */
+    int getIcuBeds();
 };
 
 #endif
