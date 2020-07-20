0779b9eb52611eef2e0ad3e0334bd7e36616af8e
joyglitch <jli43@uoguelph.ca>
2020-07-20 16:09:39 -0400

    Set up app layout

--- tool/map.py
+++ tool/map.py
@@ -1,8 +1,34 @@
 import dash
-from dash.dependencies import Output, Input
+from dash.dependencies import Output, Input, State
+from dash.exceptions import PreventUpdate
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly
 import random
 import plotly.graph_objs as go
-from collections import deque
+import plotly.express as px
+
+import pandas as pd
+
+app = dash.Dash(__name__)
+server = app.server
+#app.config.suppress_callback_exceptions = True
+
+def header():
+    return html.Div(
+        [
+            #include a logo?
+            html.H2("COVID-19 Simulation in Guelph"),
+        ],
+        className = "header__title",
+    )
+
+#Dash App Layout
+app.layout = html.Div(
+    children = [
+        header(),
+    ]
+)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
