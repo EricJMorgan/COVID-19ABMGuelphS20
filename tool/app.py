f31b687cf8ec71158cf089552e90689ad0c08d7a
joyglitch <jli43@uoguelph.ca>
2020-07-23 10:20:58 -0400

    Some changes to the app.py

--- tool/app.py
+++ tool/app.py
@@ -35,12 +35,45 @@ colors = {
 
 #Header of Dash app -- should include menu
 def header():
-    return html.Div(children = [
-        html.H2(children='COVID-19 Simulation in Guelph'),
+    return html.Div([
+        html.Div([
+            html.H2(
+                "COVID-19 Simulation in Guelph",
+            ),
+        ],
+        className = "one-half column",
+        id = "title",
+        style = {"width": "33%", "display": "inline-block"},
+        ),
+        html.Div([
+            html.A(
+                html.Button("Extra", id = "extra"),
+                #insert href here
+            ),
+            html.A(
+                html.Button("Learn More", id = "learn-more"),
+                #insert href here
+            ),
+        ],
+        className = "one-half column",
+        id = "menu-buttons",
+        style = {"width": "66%", "display": "inline-block"},
+        )
+    ],
+    id = "header",
+    className = "row flex-display",
+    )
+
+def buttons():
+    return html.Div([
+        html.Button("Start Simulation", id = "start"),
+        html.Button("Pause", id = "pause"),
+        html.Button("Reset", id = "reset"),
     ])
 
 def sideBar():
     return html.Div([
+        #GEOGRAPHICAL RISK SECTION
         html.H3(
             "Geographical Risks",
             className = "geo_risk_label",
@@ -49,47 +82,126 @@ def sideBar():
             "Social Distancing Severity",
             className = "social_distancing_label",
         ),
-        dcc.RangeSlider
-
-
-
+        dcc.Slider(
+            id = "social_distancing_slider",
+            min = 1,
+            max = 10,
+            value = 5,
+            marks = {
+                1: '1',
+                2: '2',
+                3: '3',
+                4: '4',
+                5: '5',
+                6: '6',
+                7: '7',
+                8: '8',
+                9: '9',
+                10: '10',
+            },
+            className = "dcc_control",
+        ),
+        html.P(
+            "Mask Compliance",
+            className = "mask_compliance_label",
+        ),
+        dcc.Slider(
+            id = "mask_compliance_slider",
+            min = 1,
+            max = 10,
+            value = 6,
+            marks = {
+                1: '1',
+                2: '2',
+                3: '3',
+                4: '4',
+                5: '5',
+                6: '6',
+                7: '7',
+                8: '8',
+                9: '9',
+                10: '10',
+            },
+            className = "dcc_control",
+        ),
+        html.P(
+            "Maintenance in Hygiene",
+            className = "maint_hygiene_label",
+        ),
+        dcc.Slider(
+            id = "maint_hygiene_slider",
+            min = 1,
+            max = 10,
+            value = 7,
+            marks = {
+                1: '1',
+                2: '2',
+                3: '3',
+                4: '4',
+                5: '5',
+                6: '6',
+                7: '7',
+                8: '8',
+                9: '9',
+                10: '10',
+            },
+            className = "dcc_control",
+        ),
+        #SIR INPUTS
+        html.H3(
+            "Simulation Factors",
+            className = "simulation_factors_label",
+        ),
         html.P(
             "Incubation Period",
             className = "incubation_label",
         ),
-        dcc.RangeSlider(
+        dcc.Slider(
             id = "incubation_slider",
             min = 2,
             max = 14,
+            value = 7,
+            marks = {
+                2: '2',
+                3: '3',
+                4: '4',
+                5: '5',
+                6: '6',
+                7: '7',
+                8: '8',
+                9: '9',
+                10: '10',
+                11: '11',
+                12: '12',
+                13: '13',
+                14: '14',
+            },
             className = "dcc_control",
         ),
         html.P(
             "Time Between Incubation and Hospital",
             className = "incubation_hospital_label",
         ),
-        dcc.RangeSlider(
+        dcc.Slider(
             id = "incubation_hospital_slider",
             min = 1,
             max = 7,
+            value = 6,
+            marks = {
+                1: '1',
+                2: '2',
+                3: '3',
+                4: '4',
+                5: '5',
+                6: '6',
+                7: '7',
+            },
             className = "dcc_control",
         ),
-        html.P(
-            "Time Between Hospital and ICU",
-            className = ""
-        ),
-
-        html.P(
-            "Time Between ICU and Death",
-            className = ""
-        ),
-
-        html.P(
-            "Time Between Incubation and Hospital",
-            className = ""
-        ),
-
-
-    ])
+    ],
+    className = "pretty_container four columns",
+    id = "user-options",
+    )
 
 def graph():
     return html.Div([
@@ -98,14 +210,28 @@ def graph():
             id = 'graph-update',
             interval = 1000 #update every 1 second
         )
-    ])
+    ],
+    className = "pretty_container",
+    id = "graph",
+    )
 
 #Layout of Dash app
 app.layout = html.Div(
     children = [
-
         header(),
-        graph(),
+        html.Div([
+            html.Div([
+                buttons(),
+                sideBar(),
+            ],
+            style = {"width": "25%", "display": "inline-block"},
+            ),
+            html.Div([
+                graph(),
+            ],
+            style = {"width": "50%", "display": "inline-block"},
+            ),
+        ])
     ]
 )
 
