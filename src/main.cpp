cb27a722f91e9ba938f5f35561d13f00a081fd59
JamesCodes <buschj@uoguelph.ca>
2020-07-30 09:53:55 -0400

    added demo for movment

--- src/main.cpp
+++ src/main.cpp
@@ -20,8 +20,21 @@ int main(){
     srand((unsigned int)time(NULL));
 
     Simulation sim = Simulation("demographicGuelph.csv");
+    for(int i = 0; i < 5; i++){
+        cout << sim.locationInfo->getLocationAt(i)->getSusceptibleSize() << endl;
+    }
+    cout << endl;
+    
     sim.simulateTimeStep();
+    for(int i = 0; i < 5; i++){
+        cout << sim.locationInfo->getLocationAt(i)->getSusceptibleSize() << endl;
+    }
+    cout << endl;
     sim.simulateTimeStep();
+    for(int i = 0; i < 5; i++){
+        cout << sim.locationInfo->getLocationAt(i)->getSusceptibleSize() << endl;
+    }
+    cout << endl;
     // for(int i = 0; i < 131805; i++){
     //     cout << sim.getAgentAt(i).agentToString() << endl;
     // }
