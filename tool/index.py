46b45c691fe7faaebdf8c8010d55ff13e5bdc51d
joyglitch <jli43@uoguelph.ca>
2020-08-19 11:34:54 -0400

    Changing some lines around

--- tool/index.py
+++ tool/index.py
@@ -1,3 +1,4 @@
+#Import
 import dash
 import dash_bootstrap_components as dbc
 import dash_html_components as html
@@ -11,11 +12,8 @@ import math
 import plotly
 import plotly.graph_objs as go
 import plotly.express as px
-
-import pandas as pd
-# import geopandas as gpd
-import matplotlib.pyplot as plt
 from plotly.tools import mpl_to_plotly
+import matplotlib.pyplot as plt
 
 ############################################################
 
