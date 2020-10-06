c831f1108ad4b42624d812ba8ffa115a25b1e302
JamesCodes <buschj@uoguelph.ca>
2020-10-06 07:12:19 -0400

    set all values in movment to 0

--- include/transportation.hh
+++ include/transportation.hh
@@ -1,7 +1,7 @@
 /****************
  * COVID-19ABMGuelphS20
- * 30/09/20
- * ver 1.01
+ * 06/10/20
+ * ver 1.02
  * 
  * This is the header file for the transportation class. It is used to decide where each agent will move at any given point.
  * The factors that affect this range from time, day, and age. It also initilizes the array of locations and places agents in inital starting areas
