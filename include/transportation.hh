11b72b55514b758f5818ec9db35a8392eb3eb136
JamesCodes <buschj@uoguelph.ca>
2020-10-13 11:58:23 -0400

    updated header comments

--- include/transportation.hh
+++ include/transportation.hh
@@ -1,7 +1,7 @@
 /****************
  * COVID-19ABMGuelphS20
- * 06/10/20
- * ver 1.02
+ * 13/10/20
+ * ver 1.03
  * 
  * This is the header file for the transportation class. It is used to decide where each agent will move at any given point.
  * The factors that affect this range from time, day, and age. It also initilizes the array of locations and places agents in inital starting areas
