3a28e5bb268bf14c101886858052e30ab6b7b6ce
EricJMorgan <ericmorgan@rogers.com>
2020-10-14 11:11:57 -0700

    Added new age settings and modularity

--- tool/element.py
+++ tool/element.py
@@ -60,21 +60,21 @@ def make_slider(label, id_tag, slider_value, minimum, maximum, step_value, start
         }),
     ])
 
-#Geographical Risk Sliders (4)
+# Mitigation Risk Sliders (4)
 quarantine = make_slider("Quarantine", "Q_slider", "Q_slider_value", 0, 100, 1, 75)
 social_distance = make_slider("Social Distancing Severity", "SD_slider", "SD_slider_value", 0, 10, 1, 5)
 mask_compliance = make_slider("Mask Compliance", "MC_slider", "MC_slider_value", 0, 100, 1, 75)
 hygiene_maint = make_slider("Hygiene Maintenance", "HM_slider", "HM_slider_value", 0, 100, 1, 60)
 
-#Geographical Tab
-geo_tab = dbc.Card([
+# Mitigation Strategies Tab
+mitigation_tab = dbc.Card([
     dbc.CardBody([
         html.Div([quarantine, html.Div(id='quarantine_container')]),
         html.Div([social_distance, html.Div(id='social_distance_container')]),
         html.Div([mask_compliance, html.Div(id='mask_compliance_container')]),
         html.Div([hygiene_maint, html.Div(id='hygiene_maint_container')]),
     ],
-    className="mt-1", id="geo_tab",
+    className="mt-1", id="mitigation_tab",
     )
 ])
 
@@ -88,6 +88,36 @@ entertainment = make_slider("Entertainment", "ent_slider", "ent_slider_value", 0
 health = make_slider("Health", "health_slider", "health_slider_value", 0, 1, 0.1, 0.9)
 placeofworship = make_slider("Place of Worship", "poworship_slider", "poworship_slider_value", 0, 1, 0.1, 0.8)
 residential = make_slider("Residential Areas", "res_slider", "res_slider_value", 0, 1, 0.1, 0.5)
+
+# Age specific COVID Risk Sliders
+deathChance = make_slider("Death Chance", "dc_slider", "dc_slider_value", 0, 1, 0.1, 0.2)
+recoveryTime = make_slider("Recovery Time", "recov_slider", "recov_slider_value", 0, 128, 1, 20)
+# Mitigation Use for Age group
+socialDistancingUse = make_slider("Social Distancing", "socialDis_slider", "socialDis_slider_value", 0, 1, 0.1, 0.5)
+maskUse = make_slider("Mask Wearing", "maskUse_slider", "maskUse_slider_value", 0, 1, 0.1, 0.5)
+hygieneUse = make_slider("Personal Hygiene", "hygieneUse_slider", "hygieneUse_slider_value", 0, 1, 0.1, 0.5)
+isolationUse = make_slider("Isloation Rate", "isolationRate_slider", "isolationRate_slider_value", 0, 1, 0.1, 0.5)
+
+# General Tab
+generalSettings_tab = dbc.Card([
+    dbc.CardBody([
+        html.Div([deathChance, html.Div(id='deathChance_container')]),
+        html.Div([recoveryTime, html.Div(id='recovery_container')]),
+    ],
+    className="mt-1", id="genSettings_tab",
+    )
+])
+# Mitigation Strategy effectiveness Tab
+mitAgeSettings_tab = dbc.Card([
+    dbc.CardBody([
+        html.Div([socialDistancingUse, html.Div(id='socialDis_container')]),
+        html.Div([maskUse, html.Div(id='maskUse_container')]),
+        html.Div([hygieneUse, html.Div(id='hygieneUse_container')]),
+        html.Div([isolationUse, html.Div(id='isolationRate_container')])
+    ],
+    className="mt-1", id="mitAgeSettings_tab",
+    )
+])
 
 #Location Tab
 loc_tab = dbc.Card([
@@ -106,12 +136,48 @@ loc_tab = dbc.Card([
     )
 ])
 
-#Tabs
+# Age group setting tabs
+ageDropdown = dcc.Dropdown(
+    id='age-dropdown',
+    options=[
+        {'label': '0-4', 'value': '0-4'},
+        {'label': '5-9', 'value': '5-9'},
+        {'label': '10-14', 'value': '10-14'},
+        {'label': '15-19', 'value': '15-19'},
+        {'label': '20-24', 'value': '20-24'},
+        {'label': '25-29', 'value': '25-29'},
+        {'label': '30-34', 'value': '30-34'},
+        {'label': '35-39', 'value': '35-39'},
+        {'label': '40-44', 'value': '40-44'},
+        {'label': '45-49', 'value': '45-49'},
+        {'label': '50-54', 'value': '50-54'},
+        {'label': '55-59', 'value': '55-59'},
+        {'label': '60-64', 'value': '60-64'},
+        {'label': '65-69', 'value': '65-69'},
+        {'label': '70-74', 'value': '70-74'},
+        {'label': '75-79', 'value': '75-79'},
+        {'label': '80-84', 'value': '80-84'},
+        {'label': '85+', 'value': '85+'}
+    ],
+    clearable=False,
+    searchable=False,
+    placeholder="Select an age range..."
+)
+
+# Settings tabs in the global settings
 tabs = dbc.Tabs([
-    dbc.Tab(geo_tab, label="Geographical Risks", tab_id="geo_tab"),
+    dbc.Tab(mitigation_tab, label="Geographical Risks", tab_id="mitigation_tab"),
     dbc.Tab(loc_tab, label="Location Risks", tab_id="loc_tab"),
     ],
-    id="tabs", active_tab="geo_tab", persistence=True, persistence_type='session',
+    id="tabs", active_tab="mitigation_tab", persistence=True, persistence_type='session',
+)
+
+# Settings tabs in the age specific settings
+ageTabs = dbc.Tabs([
+    dbc.Tab(generalSettings_tab, label="General Settings", tab_id="generalSettings_tab"),
+    dbc.Tab(mitAgeSettings_tab, label="Mitigation Use Settings", tab_id="mitAgeSettings_tab")
+    ],
+    id="ageTabs", active_tab="generalSettings_tab", persistence=True, persistence_type='session',
 )
 
 #Function to make graph
